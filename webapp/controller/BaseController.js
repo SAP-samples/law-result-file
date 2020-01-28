bade0ff660b55d519ec92a973b2fabd33ff927a9
Bernd Gerlach-de Campos <bernd.gerlach-de.campos@sap.com>
2020-01-28 09:54:30 +0100

    Adjust header view (dynamicly render elements)
    Also adjust layout system view
    
    Change-Id: I38da475b00d9d780605ef3c15fc9f4ca85d60c3f

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -3,11 +3,15 @@ sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast",
-	"sap/base/Log"
-], function (Controller, UIComponent, JSONModel, MessageToast, Log) {
+	"sap/base/Log",
+	"../model/formatter"
+], function (Controller, UIComponent, JSONModel, MessageToast, Log, formatter) {
 	"use strict";
 
 	var _oBundle; // holds the resource bundle for text translation
+	// var _blockSelector; // holds the dropdown box
+	var _translatableTexts;	// holds the set of texts where either 'tag' should  match a tag or a provided text should start with 'innerHTML', or both.
+	var _i18nBundle; // holds the resource bundle for text translation
 
 	return Controller.extend("sap.support.zglacelx.controller.BaseController", {
 		onInit: function () {
@@ -164,19 +168,19 @@ sap.ui.define([
 				node._tagLineStart = lastPos + 1;
 				node._tagLineEnd = node._tagLineStart;
 				node._tagClass = "leaf";
-				node._tagDepth = lastDepth + 1; // we do not want to return this change				
+				node._tagDepth = lastDepth + 1; // we do not want to return this change						
 				if (node.parentNode && node.parentNode._tagMetaBlockAssociationName === "Header") {
 					node._tagMetaBlockAssociationName = "Header";
-					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;
+					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;					
 				} else if (node.parentNode && node.parentNode._tagMetaBlockAssociationName === "Systems") {
 					node._tagMetaBlockAssociationName = "Systems";
-					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;
+					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;					
 				} else if (node.parentNode && node.parentNode._tagMetaBlockAssociationName === "Parts") {
 					node._tagMetaBlockAssociationName = "Parts";
-					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;
+					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;					
 				} else if (node.parentNode && node.parentNode._tagMetaBlockAssociationName === "Results") {
 					node._tagMetaBlockAssociationName = "Results";
-					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;
+					node._tagMetaBlockAssociationHook = node.parentNode._tagMetaBlockAssociationHook;					
 				}
 				return {
 					lPos: node._tagLineStart,
@@ -213,7 +217,7 @@ sap.ui.define([
 					}
 				} else if (node.parentNode && node.parentNode._tagMetaBlockAssociationName === "Measurement" && node.tagName === "Header") {
 					node._tagMetaBlockAssociationName = "Header";
-					node._tagMetaBlockAssociationHook = node;
+					node._tagMetaBlockAssociationHook = node;					
 				} else if (node.parentNode && node.parentNode._tagMetaBlockAssociationName === "Measurement" && node.tagName === "Systems") {
 					node._tagMetaBlockAssociationName = "Systems";
 					node._tagMetaBlockAssociationHook = node;
@@ -255,6 +259,216 @@ sap.ui.define([
 
 				return _oCallValues;
 			}
+		},
+
+		_getCodeSelector: function(oBlockSelector) { 		// _mainModelRaw = this._oModel.getData().children[0];
+			_oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var _mainModelRaw = this._oModel.getData().children[0];
+			
+			try {
+				if (oBlockSelector != null && oBlockSelector.getItems() != null && oBlockSelector.getItems().length > 0) {									
+					return oBlockSelector;
+				}
+			} catch (err) {
+				// not properly initialized, will be done now
+			}
+
+			// oBlockSelector = new sap.m.Select("blockSelector");
+			oBlockSelector.setShowSecondaryValues(true);
+			
+			var nextItem, mainText, addText, i;
+
+			/* xml.dropdown.header.text=Header
+			xml.dropdown.system.text=System {0}
+			xml.dropdown.part.text=Part {0}
+			xml.dropdown.result.text=Result {0}
+			xml.dropdown.line.text=Line {0} */
+
+			// build Header entry
+			mainText = jQuery.sap.formatMessage(_oBundle.getText("xml.dropdown.line.text"), _mainModelRaw._tagMeasurementHeaderHook._tagLineStart);
+			addText = _oBundle.getText("xml.dropdown.header.text");
+			nextItem = new sap.ui.core.ListItem( { key: "header", text: mainText, additionalText: addText } );
+			oBlockSelector.addItem(nextItem);
+
+			// build System entries
+			for (i = 0; i < _mainModelRaw._tagMeasurementSystemsHook.childElementCount; i++) {
+				mainText = jQuery.sap.formatMessage(_oBundle.getText("xml.dropdown.line.text"), _mainModelRaw._tagMeasurementSystemsHook.children[i]._tagLineStart);
+				addText = jQuery.sap.formatMessage(_oBundle.getText("xml.dropdown.system.text"), i);
+				nextItem = new sap.ui.core.ListItem( { key: "system/" + i, text: mainText, additionalText: addText } );
+				oBlockSelector.addItem(nextItem);
+			}
+			
+			// build Part entries
+			for (i = 0; i < _mainModelRaw._tagMeasurementPartsHook.childElementCount; i++) {
+				mainText = jQuery.sap.formatMessage(_oBundle.getText("xml.dropdown.line.text"), _mainModelRaw._tagMeasurementPartsHook.children[i]._tagLineStart);
+				addText = jQuery.sap.formatMessage(_oBundle.getText("xml.dropdown.part.text"), i);
+				nextItem = new sap.ui.core.ListItem( { key: "part/" + i, text: mainText, additionalText: addText } );
+				oBlockSelector.addItem(nextItem);
+			}
+			
+			// build result entries
+			for (i = 0; i < _mainModelRaw._tagMeasurementResultsHook.childElementCount; i++) {
+				mainText = jQuery.sap.formatMessage(_oBundle.getText("xml.dropdown.line.text"), _mainModelRaw._tagMeasurementResultsHook.children[i]._tagLineStart);
+				addText = jQuery.sap.formatMessage(_oBundle.getText("xml.dropdown.result.text"), i);
+				nextItem = new sap.ui.core.ListItem( { key: "resultid/" + i, text: mainText, additionalText: addText } );
+				oBlockSelector.addItem(nextItem);
+			}
+			return oBlockSelector;
+
+		},
+
+		 /* returns a title for a given combination of an engine and a metric number. Accepts 
+			engine with leading 0 or ENG(C/S/*) and metric with leading 0 or "UNT" 
+		 // File format:	"unitsList": [
+								{ "engine": "100",    "units": [ 
+            						{ "unit": "50", "unitName": "PA Master Records" }, 
+		 */ 	
+		//								GenericId	USRC0000000006	true					USRC or ENGC or null
+		_getTagTranslation: function (	tag,		innerHtml,		getMulti,	count,		context) {
+			_translatableTexts = this.getOwnerComponent().getModel("trans").getData().trans;
+
+			// is there a translation for the tag?
+			var i;
+			var displayText, start, len;
+			var transEntry, transTag, transTxt;
+			if (tag && tag !== "") {
+				if (_translatableTexts && _translatableTexts.length > 0) {
+					for (i = 0; i < _translatableTexts.length; i++) {
+						transEntry = _translatableTexts[i];
+						transTag = transEntry.tag;
+						transTxt = transEntry.innerHTML;
+						if (transTag && transTag !== "") {
+							if ((tag === "Attribute2" || tag === "ATTRIBUTE2") && (!context || context === "")) {
+								// debugger;
+							}
+							if (tag.toUpperCase().startsWith(transTag.toUpperCase())) {
+								// If multiple uiTexts exist, find the proper one: by using the firstTagHtml value
+								if (transEntry.uiText && typeof transEntry.uiText !== "string") {
+									if (transEntry.uiText.length > 0) {
+										for (var k=0; k < transEntry.uiText.length; k++) {
+											if ( (typeof transEntry.uiText[k].context === 'string' && typeof transEntry.uiText[k].uiText === 'string' && 
+													transEntry.uiText[k].context.startsWith(context))
+												|| (!context &&  !transEntry.uiText[k].context)) 
+											{
+												if (getMulti && transEntry.uiText[k].uiTextMulti) {
+													displayText = transEntry.uiText[k].uiTextMulti;
+												} else {
+													displayText = transEntry.uiText[k].uiText;
+												}
+												displayText = this._translate(displayText);
+												// format (replace {0} placeholders with numbers)
+												displayText = this._formatTranslation(displayText,	count);
+												return displayText;
+											} 
+										}
+										if (!displayText) {
+											// no translation found
+											// displayText = tag;
+											// debugger;
+										}
+									} else {
+										// no translation found
+										// displayText = tag;
+										// debugger;
+									}
+									break; 
+								} else {
+									// check if also the innerHTML must match
+									if (transTxt && transTxt !== "") {
+										if (innerHtml.toUpperCase().startsWith(transTxt.toUpperCase())) {
+											if (getMulti) {
+												displayText = transEntry.uiTextMulti;
+											} else {
+												displayText = transEntry.uiText;
+											}
+											displayText = this._translate(displayText);
+				
+											// format (replace {0} placeholders with numbers)
+											if (transEntry.number && transEntry.number === "useHtml") {
+												start = transTxt.length;
+												len = innerHtml.length - start;
+												displayText = this._formatTranslation(	displayText,		// e.g. "- Type {0}",
+																				innerHtml.substr(start, len)
+																			);
+											} else if (transEntry.number && transEntry.number === "formatTime") {
+												count = formatter.formatTime(count);
+												displayText = this._formatTranslation(
+													displayText,
+													count);
+											}
+											return displayText;
+										}
+									} else {
+										if (getMulti && transEntry.uiTextMulti) {
+											displayText = transEntry.uiTextMulti;
+										} else {
+											displayText = transEntry.uiText;
+										}
+										displayText = this._translate(displayText);
+										displayText = this._formatTranslation(displayText,	count);
+										return displayText;
+									}
+								}
+							} // else: nothing to do, for next will try next translation tag
+						} else { 
+							// no tag provided, so an innerHTML is expected to match
+							transTxt = transEntry.innerHTML;
+							if (transTxt && transTxt !== "") {
+								if (innerHtml.toUpperCase().startsWith(transTxt.toUpperCase())) {
+									if (getMulti) {
+										displayText = transEntry.uiTextMulti;
+									} else {
+										displayText = transEntry.uiText;
+									}
+									displayText = this._translate(displayText);
+		
+									// format (replace {0} placeholders with numbers)
+									if (transEntry.number && transEntry.number === "useHtml") {
+										start = transTxt.length;
+										len = innerHtml.length - start;
+										displayText = this._formatTranslation(	displayText,		// e.g. "- Type {0}",
+																		innerHtml.substr(start, len)
+																	);
+									}
+									return displayText;
+								}
+							} else {
+								// debugger;
+								// console.log("Entry in tagsTranslations.json file with neither 'tag' nor 'innerHTML' entry");
+							}
+						} 
+					} // else: no translation found, try innerHTML part
+				} else {
+					return this._translate("i18n>all.unspecified.text");
+				}
+			} 
+			return this._translate("i18n>all.unspecified.text");
+		},
+
+		_translate: function (text) {
+			_i18nBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+
+			if (!text) { 
+				return _i18nBundle.getText("all.unspecified.text"); // =[unspecified]
+			}
+			if (text.startsWith("i18n>")) {
+				text = text.substr(5, text.length);
+				var translation = _i18nBundle.getText(text);
+				if (translation) {
+					return translation;
+				} else {
+					return "[!" + text + "]";
+				}
+			} else {
+				// the text might already be translated, e.g. in the tagsTranslation.json file
+				// do NOT REMOVE this part, unless you replace all translations in the tagsTranslation.json with i18n> bundles
+				return text;
+			}
+		},
+
+		_formatTranslation: function (text, firstNumStr, secondNumStr, thirdNumStr) {
+			var newText = jQuery.sap.formatMessage(text, firstNumStr, secondNumStr, thirdNumStr);
+			return newText;
 		}
 	});
 });
--- webapp/controller/Header.controller.js
+++ webapp/controller/Header.controller.js
@@ -3,9 +3,11 @@ sap.ui.define([
 	"../model/formatter",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
-	"./layout/EqualWidthColumns"
+	"./layout/EqualWidthColumns",
+	"./layout/ResultLine",
+	"./layout/ClearLine"
 
-], function (BaseController, formatter, Filter, FilterOperator, EqualWidthColumns) {
+], function (BaseController, formatter, Filter, FilterOperator, EqualWidthColumns, ResultLine, ClearLine) {
 	"use strict";
 
 	return BaseController.extend("sap.support.zglacelx.controller.Header", {
@@ -17,21 +19,85 @@ sap.ui.define([
 			this.oView.setModel(this._oModel);
 			this.sCoreBindingPath = "/Header/";
 
-			// bind form
-			var _oHeaderForm = this.oView.byId("headerForm");
-			_oHeaderForm.bindElement(this.sCoreBindingPath);
-
 			// set code editor context
 			var _mainModelRaw = this._oModel.getData().children[0];
 			var _rawSystemData = _mainModelRaw._tagMeasurementHeaderHook;
 			var _oCodeEditor = this.byId("headerCodeEditor");
 			this.buildEditorContext(_rawSystemData, _oCodeEditor);
+
+			// for navigation buttons
+			this.iPartsCount = this._oModel.getData().children[0]._tagMeasurementPartsHook.childElementCount;
+			this.iSystemsCount = this._oModel.getData().children[0]._tagMeasurementSystemsHook.childElementCount;
+
+			var ddList = this.oView.byId("drop");
+			ddList = this._getCodeSelector(ddList);
+			ddList.setSelectedKey("header");
+			
+			var porpPanel = this.oView.byId("propSec");			
+			var panL = new sap.ui.layout.VerticalLayout( { width: "100%" }); 
+			
+			var section, displayText, title;
+			
+			for (var j = 0; j < this._oModel.getData().children[0]._tagMeasurementHeaderHook.childElementCount; j++) {
+				section = this._oModel.getData().children[0]._tagMeasurementHeaderHook.children[j];
+
+				if (section.childElementCount > 0) {
+					title = new ClearLine({ style: "elxCL1" }); 
+					panL.addContent(title);
+
+					title = new sap.m.Title ({ text: section.tagName, level: "H3" });
+					title.addStyleClass("sapUiTinyMarginTop");
+					panL.addContent(title);
+
+					if (section.childElementCount > 0)
+					for (var i = 0; i < section.childElementCount; i++) {
+						displayText = this._getTagTranslation (section.children[i].tagName,	section.tagName, false,	section.children[i].innerHTML, null); 
+
+						panL.addContent(
+							new ResultLine ({
+								label: displayText,
+								tag: section.children[i].tagName,
+								text: section.children[i].innerHTML,
+								skipTopMargin: true}) 
+						);	
+						porpPanel.addContent(panL);
+					}
+				}
+			}
+		},
+
+		_writeAllPropertyLines: function (mainArea, _result, partIdx) {
+			mainArea.addContent(
+				new ResultLine ({
+					label: this._translate("i18n>part.page.index.text"),
+					tag: "",
+					text: partIdx,
+					skipTopMargin: false }) );
+
+			var resLine = 0;
+			var labelUiText;
+
+			while (resLine < _result.children.length) {
+				var curResult = _result.children[resLine];
+				if (curResult) {
+					labelUiText = this._getTagTranslation(curResult.tagName, curResult.textContent);
+					if (typeof labelUiText !== "string") {
+						labelUiText = curResult.tagName;
+					}
+					labelUiText = this._translate(labelUiText);
+					mainArea.addContent(
+						new ResultLine ({
+							// title: labelUiText,
+							label: labelUiText,
+							tag: curResult.tagName,
+							text: curResult.innerHTML,
+							skipTopMargin: true }) );
+				}
+				resLine++;
+			}
 		},
 
 		onAfterRendering: function() {
-			var timeText = this.byId("time").getText();
-			timeText = formatter.formatTime(timeText);
-			this.byId("time").setText(timeText);
 		},
 
 		navToNext: function () {
@@ -42,6 +108,26 @@ sap.ui.define([
 
 		onElements: function () {
 			this.oRouter.navTo("elements");
-		}
+		},
+
+		navToLastSystem: function() {
+			var _iPartIndex = this.iPartsCount - 1;
+			var _iSysIndex = this._getCorrespondingSystem(_iPartIndex);
+			if (_iPartIndex <= this.iPartsCount && _iSysIndex >= 0 && _iSysIndex <= this.iSystemsCount) {
+				this.oRouter.navTo("part", {
+					partIndex: _iPartIndex,
+					sysIndex: _iSysIndex
+				});
+			}
+		},
+
+		onNavLoad: function() {
+			this.oRouter.navTo("intro");
+		},
+
+		onNavAll: function() {
+			this.oRouter.navTo("elements");
+		},
+
 	});
 });
